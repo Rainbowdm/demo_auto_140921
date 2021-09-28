092735110f2d98f766094bb24ff72a12ba0b8fd6
Rainbowdm <dim8035@gmail.com>
2021-09-28 20:17:30 +0300

    Simple tests for Page Object / Xpath locator

---
+++ src/test/java/ui/registrationTests/RegistrationTests.java
@@ -0,0 +1,75 @@
+package ui.registrationTests;
+
+import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import ui.pages.MainPage;
+import ui.pages.RegistrationPage;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+public class RegistrationTests {
+
+    private WebDriver webDriver;
+    private Logger logger = Logger.getLogger(getClass());
+
+    public MainPage mainPage;
+    public RegistrationPage registrationPage;
+
+    @Before
+    public void setUp() {
+        logger = Logger.getLogger(getClass());
+        File fileChromeDriver = new File("./drivers/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", fileChromeDriver.getAbsolutePath());
+        webDriver = new ChromeDriver();
+        logger.info("Get chromeDriver");
+        webDriver.manage().window().maximize();
+        webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        logger.info("browser was opened");
+        mainPage = new MainPage(webDriver);
+        registrationPage = new RegistrationPage(webDriver);
+    }
+
+    @After
+    public void tearDown() {
+        webDriver.quit();
+    }
+
+    private final String EMAIL = "54fdgdfgdfg34@gmail.com";
+    private final String FIRST_NAME = "Dima";
+    private final String LAST_NAME = "Hulak";
+    private final String PASSWORD = "qwerty123";
+    private final String STREET = "street New York, 12";
+    private final String CITY = "New York";
+    private final int STATE = 32;
+    private final String POST_CODE = "12345";
+    private final String MOBILE_PHONE = "1234567890";
+
+    @Test
+    public void registrationTest() {
+        mainPage.openUrl("http://automationpractice.com/");
+        registrationPage.openSignInPage();
+        registrationPage.inputEmailCreate(EMAIL);
+        registrationPage.submitButtonCreate();
+        registrationPage.inputCustomerFN(FIRST_NAME);
+        registrationPage.inputCustomerLN(LAST_NAME);
+        registrationPage.inputEmail(EMAIL);
+        registrationPage.inputPassword(PASSWORD);
+        registrationPage.inputFirstName(FIRST_NAME);
+        registrationPage.inputLastName(LAST_NAME);
+        registrationPage.inputStreet(STREET);
+        registrationPage.inputCity(CITY);
+        registrationPage.selectState(STATE);
+        registrationPage.inputPostCode(POST_CODE);
+        registrationPage.inputMobilePhone(MOBILE_PHONE);
+        registrationPage.inputAlias(EMAIL);
+        registrationPage.clickSubmitAccount();
+        Assert.assertEquals("MY ACCOUNT",
+                registrationPage.checkTitle());
+    }
+}
